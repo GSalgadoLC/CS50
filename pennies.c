74fee5b264c2ca7b227f0923faf6336c5921955e
GSalgadoLC <98500704+GSalgadoLC@users.noreply.github.com>
2022-03-09 23:50:04 -0800

    Add files via upload

---
+++ pennies.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+#include <cs50.h>
+
+int main(void)
+{
+    float amount = get_float("Dollar amount:");
+    int penniers = round(amount * penniers);
+    printf("Pennies %i \n, penniers");
+}
