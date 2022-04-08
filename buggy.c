786796ce113e3e24faa5c3fc51e11f749cc81534
GSalgadoLC <98500704+GSalgadoLC@users.noreply.github.com>
2022-04-08 16:22:40 -0700

    Create buggy.c

---
+++ buggy.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World \n");
+    
+    for (int i=0; i <=3; i++){
+        printf("i is %i\n", i);
+        printf("#\n");
+    }
+    
+
+    return 0;
+}
